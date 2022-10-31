11b204802063eca6d00513fd634ce71220a8796a
sahildhimandesigner <sahil@ucreate.co.in>
2022-10-31 17:47:41 +0530

    created the new function for calculate price and remove the old code

--- mycart.js
+++ mycart.js
@@ -12,24 +12,23 @@ function cartProductList(){
     })
 }
 
-function cartProductListItem(cartData){
-    // let subTotal = 0;    
-    let loadTableData = "";
-    // cartData = cartData.filter(element => element.proName);   
-
-    cartData.forEach((productItem) => {     
+function cartProductListItem(cartData){     
+    let loadTableData = "";    
+    
+    cartData.forEach((productItem) => {
+        
         let proudctQty = productItem.qty ? productItem.qty : '1';
         let productPrice = productItem.qty * productItem.proPrice;
         loadTableData += `
-            <tr>
+            <tr class="table-row">
             <td valign="middle"><input type="checkbox" class="cart-product" value="1" id="cartPro_${productItem.id}" onchange="selectItem(${productItem.id})" /></td>
             <td valign="middle"><img src="${productItem.proImg ? productItem.proImg : noImage}" style="width:100px;" class="card-img-top" alt="..."></td>
             <td valign="middle">${productItem.proName}</td>
             <td valign="middle">${productItem.proDesc}</td>
-            <td valign="middle">${productItem.proPrice}</td>
+            <td valign="middle" class="product-price">${productItem.proPrice}</td>
             <td valign="middle">${productItem.proCat}</td>
             <td valign="middle">
-                <input type="number" class="col-md-2" id="product-${productItem.id}" onkeyup="getQuantity(${productItem.proPrice}, ${productItem.id})" value="${proudctQty}" />
+                <input type="number" class="col-md-2 qty" id="product-${productItem.id}" onchange="getQuantity(${productItem.proPrice}, ${productItem.id})" value="${proudctQty}" />
             </td>      
             <td valign="middle">
                 <button class="border-0" onclick="deletProduct(${productItem.id})">
@@ -40,52 +39,41 @@ function cartProductListItem(cartData){
             </td>
             </tr>
             <input type="hidden" class="pro-cost" value="${productPrice}" id="cost-${productItem.id}" />
-        `
-        
-        //parseInt use to conver string to number
-        // price = productPrice        
-        // subTotal = subTotal + price;       
-        // vat(subTotal);
-        // document.getElementById("totalPrice").innerHTML = subTotal;                
-        document.getElementById("cartListProduct").innerHTML = loadTableData;        
+        `        
     })    
 
+    document.getElementById("cartListProduct").innerHTML = loadTableData; 
+    calCulatePrice();    
     cartValue = cartData.length;
     myCart(cartValue)
 }
 myCart()
 
+function calCulatePrice(){    
+    let proTotalPrice = 0;
+    let tableRow = document.querySelectorAll(".table-row");
+    tableRow.forEach((tablData) => {
+        if(tablData.querySelector(".cart-product:checked")){
+           let productPrice = tablData.querySelector('.product-price').innerHTML;           
+           let productQty = tablData.querySelector('.qty').value;
+           proTotalPrice += parseInt(productPrice) * parseInt(productQty);              
+        }
+    })    
+    document.getElementById("totalPrice").innerHTML = proTotalPrice;
+    console.log(tableRow, 'tableRow')
+    vat(proTotalPrice)
+}
 
 function selectItem(selectedProId){
-
-    let ddd = document.getElementById("total_value").value;
-
-    let newTotal = parseInt(ddd);
-    
-    let getTotalPrice = document.getElementById("cost-"+selectedProId).value;          
-    
-    let converTotalPrice = parseInt(getTotalPrice);
-    
-    let getProduct = document.getElementById("cartPro_"+selectedProId);    
-    
-    if(getProduct.checked){
-        newTotal += converTotalPrice;        
-    }else if (!getProduct.checked) {
-        newTotal -= converTotalPrice;
-    }    
-
-    document.getElementById("total_value").value = newTotal;
-
-    document.getElementById("totalPrice").innerHTML = newTotal;
-    vat(newTotal)
+    calCulatePrice(selectedProId);
 }
 
 
 
-function getQuantity(proPrice, id){
-    let getProQty = document.getElementById("product-" + id).value;        
+function getQuantity(proPrice, id){    
+    let getProQty = document.getElementById("product-" + id).value;
     let currentQty = parseInt(getProQty);
-    
+        
     const updateProQty = {
         method : "PATCH",
         headers: { 'Content-Type': 'application/json' },
@@ -94,9 +82,8 @@ function getQuantity(proPrice, id){
 
     fetch('https://api-generator.retool.com/Bl2mIo/data/' + id, updateProQty)
     .then(response => response.json())
-    .then(data => cartProductListItem(data))
-    cal(proPrice, id);                
-    calPrice();
+    .then(data => cartProductListItem(data))    
+    calCulatePrice()
 }
 
 function vat(price){
@@ -110,90 +97,17 @@ function myCart(cartValue){
     document.getElementById("myCart").innerHTML = cartValue ? cartValue : "";
 }
 
-function calPrice(getDetails){
-    let newSub = 0;    
-    if(getDetails){
-        let newValue = document.getElementById("cost-" +getDetails).value;
-        
-        //get the all product item for iterate
-        let getId = document.querySelectorAll('.pro-cost');
-        
-        //adding all the product value into newsub var
-        for(k=0; k < getId.length; k++){        
-            newSub += parseInt(getId[k].value);
-        }
-
-        let dd = newSub - newValue;
-
-        document.getElementById("totalPrice").innerHTML = dd;
-        vat(dd);
-    }    
-    else {
-        //get the all product item for iterate
-        let getId = document.querySelectorAll('.pro-cost');
-
-        //adding all the product value into newsub var
-        for(k=0; k < getId.length; k++){        
-            newSub += parseInt(getId[k].value);
-        }
-        document.getElementById("totalPrice").innerHTML = newSub;
-        vat(newSub);
-    }
-}
-
-function cal(proPrice, id){
-    let totalcost = 0;
-    qvalue = document.getElementById("product-" + id).value;    
-
-    let quantity =  parseInt(qvalue)
-    if(quantity > 1){
-        totalcost = (proPrice) * (quantity - 1);
-    }
-    if(quantity > 0){
-        totalcost = proPrice * quantity;
-    }
-    document.getElementById("cost-" +id).value = totalcost;
-}
-
-function deletProduct(getDetails){        
-    calPrice(getDetails)
+function deletProduct(getDetails){            
     showLoader();    
     fetch('https://api-generator.retool.com/Bl2mIo/data/'+ getDetails, {
             method: 'DELETE',
     }).then(function(){                
             hideLoader();        
             cartProductList();
-            myCart();
+            myCart();            
         }       
     )
 }
-
-// testApi();
-// function testApi(){
-//     const options = {
-// 	method: 'POST',
-// 	headers: {
-// 		'content-type': 'application/json',
-// 		'Content-Type': 'application/json',
-// 		'X-RapidAPI-Key': '4ed8005f40mshb1acace068fd8c4p18b964jsn57e9401c255a',
-// 		'X-RapidAPI-Host': 'pincode.p.rapidapi.com'
-// 	},
-// 	body: '{"searchBy":"pincode","value":110001}'
-// };
-// let tst = '';
-// fetch('https://pincode.p.rapidapi.com/', options)
-// 	.then(response => response.json())
-// 	.then(function(data){        
-//         data.forEach((userInfo) =>{
-//             console.log(userInfo)
-//             tst += `<li><span>${userInfo.pin}</span> <span>${userInfo.office}</span> <span>${userInfo.division}</span></li>`            
-//         })
-//         document.getElementById("tst").innerHTML = tst;
-//     })
-// 	.catch(err => console.error(err));
-    
-// }
-
 
 function productCheckout(){
     alert('sdfdsfsd')
