2741e81f90165bb9babcb5a6a02085844a2d0fef
sahildhimandesigner <sahil@ucreate.co.in>
2022-11-11 16:33:39 +0530

    implemented the edit product functionality

--- product.js
+++ product.js
@@ -149,11 +149,17 @@ function productList(loadData){
                 <p class="card-text">Price: ${element.product_price}</p>
                 <p>Category: ${element.product_category}</p>
                 <button onclick="addToCart(${element.id})" class="btn btn-primary">add to cart</button>
-                <button onclick="addToWhishlist()" class="btn btn-secondary">add to whislist</button>
+                
 
                 <button onclick="deleteProductBtn(${element.id})" class="btn border-0 m-0 p-0 position-absolute top-0">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
                         <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
+                    </svg>
+                </button>
+
+                <button class="btn border-0 mx-4 p-0 position-absolute" onclick="editProduct(${element.id})">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
+                        <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
                     </svg>
                 </button>
             </div>            
@@ -291,3 +297,27 @@ function deleteProductBtn(deletedProId){
         })   
     }    
 }
+
+function editProduct(id){
+    document.getElementById("edit_product_form").style.display = "block";    
+    document.getElementById("product").style.display = "none";
+    fetch('https://api-generator.retool.com/BsUw6I/data/'+id,{
+        method: 'get'
+    }).then(function(response){
+        return response.json();
+    }).then(function(productData){        
+        const productName = productData.product_name;
+        const productDescription = productData.description;
+        const productCat = productData.product_category;
+        const productPrice = productData.product_price;
+        const productImg = productData.product_image;
+        
+        document.getElementById("productName").value = productName;
+        document.getElementById("productPrice").value = productPrice;
+        document.getElementById("description").value = productDescription;
+        document.getElementById("category").value = productCat;
+        document.getElementById("product_image").value = productImg;
+    })
+}
+
+document.getElementById("edit_product_form").style.display = "none";
